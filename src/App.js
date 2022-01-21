06a93a394820e77dd51701f6b257037f630c626e
Deevanshu Kumawat <deevanshu1997@gmail.com>
2022-01-21 21:09:14 +0530

    basic css is done

--- src/App.js
+++ src/App.js
@@ -4,9 +4,7 @@ import ProjectCard from "./dashboard/problem-card/ProblemCard";
 function App() {
   return (
     <div className="App">
-
       <ProjectCard />
-
     </div>
   );
 }
--- src/dashboard/problem-card/ProblemCard.jsx
+++ src/dashboard/problem-card/ProblemCard.jsx
@@ -1,26 +1,58 @@
-const title = "deev";
-const topic = "deev";
-const difficulty = "deev";
-const platform = "deev";
-const isSolved = "deev";
-const updatedOn = "deev";
+import problemCard from "./ProblemCard.module.css";
+
+const title = "Title";
+const topic = "Topic Name";
+const difficulty = "Hard";
+const platform = "leetcode";
+const isSolved = false;
+const updatedOn = "2020-05-05";
 const ProjectCard = () => {
   return (
-    <>
-      hello
-      <div>
-        <h1>{title}</h1>
-        <h2>{topic}</h2>
-        <h3>Difficulty Level: {difficulty}</h3>
-        <h4>Updated on {updatedOn}</h4>
+    <div className={problemCard.Container}>
+      <div className={problemCard.LeftDiv}>
+        <p className={problemCard.Title}>{title}</p>
+        <div className={problemCard.CentreDiv}>
+          <p className={problemCard.Topic}>{topic}</p>
+          <p className={problemCard.Difficulty}>
+            Difficulty Level:{" "}
+            <span
+              className={
+                difficulty === "Hard"
+                  ? problemCard.Hard
+                  : difficulty === "Medium"
+                  ? problemCard.Medium
+                  : problemCard.Easy
+              }
+            >
+              {difficulty}
+            </span>
+          </p>
+        </div>
+
+        <p className={problemCard.Date}>Updated on {updatedOn}</p>
       </div>
-      <div>
-        {isSolved ? <h3>Solved</h3> : <h3>Not Solved Yet</h3>}
-        <button>View details</button>
-        <p>{platform}</p>
-        <button>Attempt Problem</button>
+      <div className={problemCard.RightDiv}>
+        {isSolved ? (
+          <p className={problemCard.Solved}>Solved</p>
+        ) : (
+          <p className={problemCard.NotSolved}>Not Solved Yet</p>
+        )}
+        <div>
+          <a href="deevanshu.dev" className={problemCard.Link}>
+            View details
+          </a>
+          <div>
+            <img
+              src={platform === "leetcode" ? "icons/leetcode.svg" : ""}
+              alt="platform_img"
+            />
+            <a href="deevanshu.dev" className={problemCard.Link}>
+              Attempt Problem
+            </a>
+          </div>
+        </div>
       </div>
-    </>
+    </div>
   );
 };
 
