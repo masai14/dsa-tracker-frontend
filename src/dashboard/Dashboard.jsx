0b125d95e23746109c3dec831c486105a904ccbb
Deevanshu Kumawat <deevanshu1997@gmail.com>
2022-01-22 09:41:43 +0530

    navbar done

--- src/dashboard/Dashboard.jsx
+++ src/dashboard/Dashboard.jsx
@@ -1,16 +1,64 @@
 import ProblemCard from "./problem-card/ProblemCard";
+import Navbar from "../navbar/Navbar";
+import dashboard from "./Dashboard.module.css";
 
 export default function Dashboard() {
   return (
-    <div className="App">
-      <ProblemCard
-        title="Title"
-        topic="Topic Name"
-        difficulty="Easy"
-        platform="leetcode"
-        isSolved={true}
-        updatedOn={Date()}
-      />
-    </div>
+    <>
+      <Navbar />
+      <div className={dashboard.Container}>
+        <div></div>
+        <div className="App">
+          <ProblemCard
+            title="Title"
+            topic="Topic Name"
+            difficulty="Easy"
+            platform="leetcode"
+            isSolved={true}
+            updatedOn="2020-05-05"
+          />
+          <ProblemCard
+            title="Title"
+            topic="Topic Name"
+            difficulty="Hard"
+            platform="leetcode"
+            isSolved={false}
+            updatedOn="2020-05-05"
+          />
+          <ProblemCard
+            title="Title"
+            topic="Topic Name"
+            difficulty="Easy"
+            platform="leetcode"
+            isSolved={true}
+            updatedOn="2020-05-05"
+          />
+          <ProblemCard
+            title="Title"
+            topic="Topic Name"
+            difficulty="Hard"
+            platform="leetcode"
+            isSolved={false}
+            updatedOn="2020-05-05"
+          />
+          <ProblemCard
+            title="Title"
+            topic="Topic Name"
+            difficulty="Easy"
+            platform="leetcode"
+            isSolved={true}
+            updatedOn="2020-05-05"
+          />
+          <ProblemCard
+            title="Title"
+            topic="Topic Name"
+            difficulty="Hard"
+            platform="leetcode"
+            isSolved={false}
+            updatedOn="2020-05-05"
+          />
+        </div>
+      </div>
+    </>
   );
 }
