8e39ded74ed64857e8bbe9e35997190f99bd6b41
AKHIL KUMAR ADEPU <akhil9394021176@gmail.com>
2022-01-22 19:08:28 +0530

    Auth provider and login page page done

--- src/index.js
+++ src/index.js
@@ -2,10 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
+import { BrowserRouter } from "react-router-dom";
+import { AuthContextProvider } from "./Contexts/AuthContextProvider";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <AuthContextProvider>
+        <App />
+      </AuthContextProvider>
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById("root")
 );
